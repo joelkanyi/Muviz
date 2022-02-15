71605a4e5511ce5e3102b6d28f7fa7a393699d82
JoelKanyi <joelkanyi98@gmail.com>
2022-02-15 16:47:23 +0300

    nested lazy column working but not working well

--- app/src/main/java/com/kanyideveloper/muviz/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/kanyideveloper/muviz/presentation/ui/MainActivity.kt
@@ -3,16 +3,21 @@ package com.kanyideveloper.muviz.presentation.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
+import com.kanyideveloper.muviz.R
 import com.kanyideveloper.muviz.presentation.NavGraphs
 import com.kanyideveloper.muviz.presentation.components.BottomNavigationBar
+import com.kanyideveloper.muviz.presentation.components.StandardToolbar
 import com.kanyideveloper.muviz.presentation.ui.theme.MuvizTheme
+import com.kanyideveloper.muviz.presentation.ui.theme.primaryGray
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import dagger.hilt.android.AndroidEntryPoint
 
