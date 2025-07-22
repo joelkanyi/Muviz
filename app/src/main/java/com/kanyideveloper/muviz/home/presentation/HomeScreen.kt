5d7407129a1f7d99ee6f8f097e478122de1f0c95
Joel Kanyi <joelkanyi98@gmail.com>
2025-07-22 17:31:40 +0300

    refactor: replace wildcard imports with explicit imports in HomeScreen.kt

--- app/src/main/java/com/kanyideveloper/muviz/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/kanyideveloper/muviz/home/presentation/HomeScreen.kt
@@ -39,7 +39,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
